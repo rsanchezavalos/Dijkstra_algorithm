63eb3b529ab341acd3a4d1ac6553d88ac0bbf49b
Roberto Sánchez Ávalos <r.sanchezavalos@gmail.com>
2018-03-19 00:38:43 +0000

    Added bib and initial commit of firts versions

---
+++ variants_c/version1/dijkstra.C
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+// funcion para conocer todos los nodos a los que se puede llegar
+// funcion para determinar el nodo más cercano
+// funcion para almacenar en un set los nodos a los que ya fuiste
+// funcion para incrementar el costo con cada decisión
